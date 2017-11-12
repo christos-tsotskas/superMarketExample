77222a9ebddb48e77ee8bb1da1f7fd3038957424
christos.tsotskas <christos.tsotskas@ts.catapult.org.uk>
2017-11-12 17:18:54 +0000

    separated tests

--- src/test/java/AveragerTest.java
+++ src/test/java/AveragerTest.java
@@ -1,17 +1,33 @@
 import org.junit.Test;
 
+import org.junit.*;
 import static org.junit.Assert.*;
 
 public class AveragerTest {
 
+    private Averager dummyAverager;
+
+    @Before
+    public void setUp() {
+        dummyAverager = new Averager(1, 1 );
+    }
 
 
     @Test
-    public void testAverages(){
+    public void testAveragesForOneAndOne(){
         double expected = 1.0;
-        double actual = 1.0;
+        double actual = dummyAverager.getAverage();
 
         assertEquals(expected, actual, 0.1);
     }
+
+    @Test
+    public void testAveragesFailsForOneAndOne(){
+        double expected = 1.6;
+        double actual = dummyAverager.getAverage();
+
+        assertNotEquals(expected, actual, 0.1);
+    }
+
 
 }

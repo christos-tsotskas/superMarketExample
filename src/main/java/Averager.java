9d532a10c12ab9e3670158864e3c7cddc26d5e88
christos.tsotskas <christos.tsotskas@ts.catapult.org.uk>
2017-11-12 16:34:53 +0000

    first commit

---
+++ src/main/java/Averager.java
@@ -0,0 +1,17 @@
+public class Averager {
+
+    private  int a,b;
+
+    public Averager(int a, int b){
+        this.a = a;
+        this.b = b;
+
+    }
+
+    public double getAverage(){
+        return (this.a + this.b ) / 2 ;
+    }
+
+
+
+}
---
+++ src/test/java/AveragerTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AveragerTest {
+
+
+    @Test
+    public void testAverages(){
+        double expected = 1.0;
+        double actual = 1.0;
+
+//        assertEqual(expected, actual);
+
+        assertEquals(expected, actual, 0.1);
+    }
+
+}

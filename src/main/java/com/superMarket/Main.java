402cdb84faa9cb842495ce43742b22f4f53fab91
christos.tsotskas <christos.tsotskas@ts.catapult.org.uk>
2017-11-12 17:58:58 +0000

    added main classes and deployment experiment

---
+++ src/main/java/com/superMarket/Main.java
@@ -0,0 +1,8 @@
+package com.superMarket;
+
+public class Main {
+    public static void main(String[] args) {
+        String password = "Blah1" ;
+        System.out.println(password);
+    }
+}
---
+++ src/main/java/com/superMarket/Offer.java
@@ -0,0 +1,4 @@
+package com.superMarket;
+
+public class Offer {
+}

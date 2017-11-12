0504222195dd5f07b6b21721634f8bbf3e0a168f
christos.tsotskas <christos.tsotskas@ts.catapult.org.uk>
2017-11-12 22:28:08 +0000

    house keeping

--- src/test/java/com/superMarket/UserInterfaceTest.java
+++ src/test/java/com/superMarket/UserInterfaceTest.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017. Christos Tsotskas
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.superMarket;
 
 import org.junit.Test;
@@ -8,8 +32,14 @@ import com.superMarket.UserInterface;
 
 import static org.junit.Assert.*;
 
+/**
+ *  Test User Interface
+ */
 public class UserInterfaceTest {
 
+    /**
+     * verify Output On A Period Of Offer With Dummy Values
+     */
     @Test
     public void verifyOutputOnAPeriodOfOfferWithDummyValues() {
         String[] commandLineArguments = {"Apples", "Milk", "Bread"};
